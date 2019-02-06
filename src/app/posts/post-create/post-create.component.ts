bd7b1e330163ae35f254d24a805aa54f9c8d8c81
Robert Pirritano <rpirritano@gmail.com>
2019-02-06 08:31:02 -0800

    added input/output

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+// standard to use "on before the action such as onAddPost()"
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+
+  onAddPost(postInput: HTMLAreaElement) {
+    // console.dir(postInput);
+    // this.newPost = postInput.value;
+    this.newPost = this.enteredValue;
+  }
+
+}

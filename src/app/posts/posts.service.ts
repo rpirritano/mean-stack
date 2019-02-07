44ad7ef7fe0b8251a3536cf257f7ecbfcdecff5d
Robert Pirritano <rpirritano@gmail.com>
2019-02-07 09:03:50 -0800

    added rxjs

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,17 +1,24 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 // alternative way to perform DI instead of adding it to app.module.ts
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
     return [...this.posts];
   }
 
+  getPostUpdateListerner() {
+    return this.postsUpdated.asObservable();
+  }
+
   addPost(title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }

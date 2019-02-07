47d8df37a2626a4d89b7031c95d16f7d947f6b82
Robert Pirritano <rpirritano@gmail.com>
2019-02-06 18:27:40 -0800

    added post model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './posts/post-create/post-list/post-list.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
